6d56b902a0066b19ec3ca770020309ab713e9868
Jayesh Patel <jayeshpatel@jayesh.local>
2016-04-23 15:43:33 +0530

    Initial Commit

---
+++ ZipDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZipDemo
+//
+//  Created by Jayesh Patel on 23/04/16.
+//  Copyright © 2016 Jayesh Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZipDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZipDemo
+//
+//  Created by Jayesh Patel on 23/04/16.
+//  Copyright © 2016 Jayesh Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
